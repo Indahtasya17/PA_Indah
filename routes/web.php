1cd4b2fe4c76d794f9db0c6b4c79dc508bcbb6a6
jrfs22 <mr.jrfs22@gmail.com>
2025-04-18 21:24:58 +0700

    add: barang import masuk

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\BarangImportController;
 use App\Http\Controllers\PelabuhanController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::prefix("pelabuhan")->group(function () {
+Route::prefix("barang-masuk")->group(function () {
     Route::get('', [PelabuhanController::class, 'index'])->name('pelabuhan');
     Route::get('create', [PelabuhanController::class, 'create'])->name('pelabuhan.create');
     Route::get('{id}/edit', [PelabuhanController::class, 'edit'])->name('pelabuhan.edit');
+});
+
+Route::prefix('barang-import')->group(function () {
+    Route::prefix('masuk')->group(function () {
+        Route::get('', [BarangImportController::class, 'index'])->name('barang-import.masuk');
+        Route::get('create', [BarangImportController::class, 'create'])->name('barang-import.masuk.create');
+    });
 });
